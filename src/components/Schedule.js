5707b9bc2bb7e0039c7b1c733b3d5c49f2c0e744
Nick Krein <nkreinmusic@gmail.com>
2016-09-23 15:57:39 -0700

    update time format to 24 hour clock

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -9,8 +9,8 @@ export default class Schedule extends Component {
   static defaultProps = {
   	timeRange: [
   		'04:00 AM', '05:00AM','06:00AM','07:00AM','08:00AM','09:00AM',
-  		'10:00AM','11:00AM','12:00PM','01:00PM','02:00PM','03:00PM',
-  		'04:00PM','05:00PM','06:00PM','07:00PM','08:00PM','09:00PM'
+  		'10:00AM','11:00AM','12:00PM','13:00PM','14:00PM','15:00PM',
+  		'16:00PM','17:00PM','18:00PM','19:00PM','20:00PM','21:00PM'
   		],
   }
 

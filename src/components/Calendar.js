e6667255169a4df6d2639b42b01e1467a23403da
Nick Krein <nkreinmusic@gmail.com>
2016-09-22 20:44:56 -0700

    Initial commit

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -9,37 +9,9 @@ export default class Calendar extends Component {
     super(props);
   }
 
-  componentDidMount() {
-  	const { calendar } = this.refs
-    const $calendar = $(calendar);
-
-  	$calendar.fullCalendar({
-  		header: {
-  			left: 'prev, next, today',
-  			center: 'title',
-        right: 'month agendaDay'
-  		},
-      views: {
-        agenda: {
-        allDaySlot: false,
-        slotDuration: '01:00:00',
-        minTime: '04:00:00',
-        maxTime: '22:00:00'
-        }
-      },
-      fixedWeekCount: false,
-      dayClick: function(date, jsEvent, view) {
-        $calendar.fullCalendar('changeView', 'agendaDay');
-        $calendar.fullCalendar('gotoDate', date);
-      } 
-  	});
-  }
-
-
-
   render() {
     return (
-      <div ref="calendar"></div>
+      <div></div>
     );
   }
 }
